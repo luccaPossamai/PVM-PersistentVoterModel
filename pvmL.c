fad523cc1874982b8fd76b8c9e2bafb92c0482f2
lpcamors <110493821+luccaPossamai@users.noreply.github.com>
2024-11-24 14:53:28 -0300

    Add files via upload

--- pvmL.c
+++ pvmL.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <mc2023.h>
 #include <lat2eps.h>
+#include <monte_carlo.h>
 
 
 void teste(void);
@@ -16,7 +17,7 @@ void atribuirPropriedades(void);
 void criarVizinhos(void);
 int bloqueado(int);
 void interacoes(void);
-unsigned long open_output_files(void);
+unsigned long generateFile(void);
 void hoshen_kopelman(int*, int*, int*);
 void verificarLiberdadeVizinhos(int, int);
 int percolates2d(int, int*);
@@ -24,25 +25,30 @@ void createPerfectLogTable(int*);
 void comecar(int);
 void iniciar_coleta(int);
 void verificarCruzamentoInterface(void);
-void save_configuration(int);
+void save_configuration(int, char*);
 void time_evolution(void);
 void write(double, int, int, int);
 void updateOldValues(void);
+void interface(void);
+int getInterfacialSizeOfCluster(int*, int*);
+void getBiggestCluster(int*, int*, int);
+void unique_cluster(void);
 
 
 /*******************************************************************/
 /**                             CONSTANTES                        **/
 /*******************************************************************/
-#define LOOPS                   200
-#define TEMPO_MAX               10000000
+#define LOOPS                   500
+#define TEMPO_MAX               1e5
 #define PASSO_TEMPO             500
-#define DELTAETA_INICIAL        1./100000
-#define L                       128
-#define N                       L * L
+#define DELTAETA_INICIAL        1./1000
+#define LSIZE                   128
+#define NSIZE                   LSIZE * LSIZE
 
 
-#define EVOLUCAO                0           // 0: NORMAL
+#define EVOLUCAO                2           // 0: NORMAL
                                             // 1: ATINGIR CONSENSO
+                                            // 2: Interface Lenght
 
 #define CONFIGURACAO_I          2           // 1 : CIRCULAR
                                             // 2 : LINEAR**
@@ -60,21 +66,24 @@ void updateOldValues(void);
 /******* Par�metros para CONFIGURACAO_I = CIRCULAR *****************/
 #define RAIO                    10          //NAO IMPLEMENTADO AINDA
 
-#define FORCE_SEED              0
+#define FORCE_SEED              1
 /******* Par�metros para CONDICAO DE CONTORNO LINEAR FIXA **********/
 #define FIXED_EXTREMES          1           //CILINDRIC
-#define FIXED_BORDERS           1           //SQUARED
+#define FIXED_BORDERS           0           //SQUARED
+#define L_INICIAL               16
+#define L_FINAL                 256
 /*******************************************************************/
-#define SAVE_CONFIG             0
-#define CONFIG_T0               21000
-#define CONFIG_DELTA_T          100
+#define SAVE_CONFIG             1
+#define CONFIG_T0               0
+#define CONFIG_DELTA_T          100000
 #define CONFIG_TF               111000//CONFIG_T0 + 10 * CONFIG_DELTA_T
 
 
 
 int *dir, *esq, *cima, *baixo, **vizinhos, *zelotes, *polaridade/*0, 1 */,
     *moveis, *quaisMoveis, *tempos, mag, ncl, zar, interfCol;
-int nMoveis0, mag0, *polaridade0, *zelotes0,  *moveis0, *quaisMoveis0;
+int nMoveis0, mag0, *polaridade0, *zelotes0,  *moveis0, *quaisMoveis0,
+    l0_int = 0, l1_int = 0, L = LSIZE, N = NSIZE;
 float *confianca0;
 int *v, nMoveis;
 double tempo,deltaEta;
@@ -83,7 +92,7 @@ unsigned int seed;
 FILE *fp1;
 
 int main(void){
-    int lps = LOOPS;
+    int lps = 200;//getIntInputFromClient("loops = ", 1, 1e4);
     if(FORCE_SEED){
         lps = 1;
     }
@@ -93,27 +102,28 @@ int main(void){
 
 void iniciar_coleta(int i){
     int j, t = 0;
-    printf("Começando a(s) %d coletas de dados. \n", i);
+    printf("  Starting %d loops for data collection. \n", i);
     for(j = 0; j < i; j++){
         t = 0 - clock();
         comecar(j);
         t += clock();
-        printf("    Conclu�do, tempo de dura��o: %.2f s \n",(double) t/CLOCKS_PER_SEC);
+        printf("  Collection concluded, duration time: %.2f seconds \n",(double) t/CLOCKS_PER_SEC);
     }
     return;
 }
 
 
 void comecar(int i){
-    printf("\n    Iniciando a %da coleta\n", i + 1);
+    printf("\n    Starting the %dth collection\n", i + 1);
     pre();
     teste();
     if(EVOLUCAO == 0) {
         time_evolution();
-    } else {
+    } else if(EVOLUCAO == 1){
         consensus();
+    } else if(EVOLUCAO == 2){
+        interface();
     }
-    printf("%d", tempos[MEDIDAS - 1]);
     post();
 }
 
@@ -149,7 +159,7 @@ void post(void){
 
 void pre(){
     seed=0;
-    seed = open_output_files();
+    seed = generateFile();
     start_randomic(seed);
 
     criarVizinhos();
@@ -212,6 +222,51 @@ void consensus(void){
     }
 }
 
+void interface(void){
+  int measures = 20;
+  float *l_arr = jmalloc(20 * sizeof(float));
+  geomProgression(l_arr, L_INICIAL, L_FINAL, measures);
+
+  char name[50];
+    L = L_INICIAL;
+    int threshold = 1e3, l0, l1, lar;
+    deltaEta = 1e-2;
+    threshold = 1e3;
+    fprintf(fp1,"#  deltaEta: %.5f\n", deltaEta);
+    fprintf(fp1,"#  threshold: %d\n", threshold);
+    fprintf(fp1,"#  L    <L0>   <L1>   <L_Ar>\n");
+    for(int i = 0; i < measures; i++){
+      L = (int) l_arr[i];
+      N = L * L;
+        sprintf(name, "%d", L);
+        criarVizinhos();
+        criarPropriedades();
+        atribuirPropriedades();
+        updateOldValues();
+        tempo = 0.0;
+        l0 = 0; l1 = 0; lar = 0;
+        while(tempo < TEMPO_MAX){
+          tempo += 1./nMoveis;
+            interacoes();
+            updateOldValues();
+            if(tempo >= threshold){
+              unique_cluster();
+              l0 = l0_int;
+              l1 = l1_int;
+              fprintf(fp1,"%d %d %d %d\n", L, l0 - L, l1 - L, lar);
+              fflush(fp1);
+
+              if(SAVE_CONFIG) save_configuration(tempo, name);
+              break;
+            }
+            if(tempo > TEMPO_MAX){
+                break;
+            }
+        }
+    }
+}
+
+
 
 void evolucao(void){
     int i, z = 0, z1 = 1, per, ti = 0, tiMax = (int) MEDIDAS - 1, oldTempo = CONFIG_T0;
@@ -224,15 +279,15 @@ void evolucao(void){
     while(ti <= tiMax){
         proxTempo = tempos[ti];
         while(tempo <= proxTempo && nMoveis > 0){
-            tempo += 1./nMoveis;
             interacoes();
+            tempo += 1./nMoveis;
             if(nMoveis == 0) tempo = tempos[tiMax];
 
             if(SAVE_CONFIG == 1){
                 if (tempo >= CONFIG_T0 && tempo <= CONFIG_TF) {
                     if(tempo > oldTempo){
                         printf("flash");
-                        save_configuration(tempo);
+                        save_configuration(tempo, "a");
                     }
                 }
             }
@@ -267,7 +322,7 @@ void write(double proxTempo, int z, int z1, int per){
     fprintf(fp1,"%.5f  %d %d %d %d %d %d %d %d\n", proxTempo, mag0, z1, z, ncl, zar, per, nMoveis0, interfCol);
     if(SAVE_CONFIG == 1){
         if (proxTempo >= CONFIG_T0 && proxTempo <= CONFIG_TF) {
-            save_configuration(proxTempo);
+            save_configuration(proxTempo, "evolution");
         }
 
     }
@@ -328,8 +383,8 @@ void interacoes(void){
 
     if(CONFIGURACAO_I == 2){
         if((FIXED_EXTREMES == 1 || FIXED_BORDERS == 1) && (pPos < L || pPos >= N - L)) return;
-        if(FIXED_BORDERS == 1 && ((pPos % (L - 1) == 0) || (pPos % L == 0))) return;
-        if((vPos < L && vIndice == 3)|| (pPos < L && vIndice == 2)) return;
+        if(FIXED_BORDERS == 1 && (((pPos % L) == L - 1) || (pPos % L == 0))) return;
+        if((vPos < L && vIndice == 3)|| (pPos < L && vIndice == 2)) return; //prevents top/bottom interaction
     }
 
     pZelote = zelotes[pPos] == 1;
@@ -539,74 +594,53 @@ void verificarCruzamentoInterface(){
 }
 
 
-//C�DIGO PEGO DO PVM ARENZON
-
-
-unsigned long open_output_files(void){
-char output_file_1[100],root_name[50];
-int ok;
-unsigned long identifier;
-
-identifier = (unsigned long) time(NULL);
-if(FORCE_SEED == 1) identifier = 1724206819;
-if (identifier%2==0) ++identifier; //odd number to be later used as the seed for randoms
-switch (CONFIGURACAO_I)
-       {
-       	case 1: sprintf(root_name,"PVM_L%d_IS%d_R%d",L,CONFIGURACAO_I,RAIO);
-		break;
-       	case 2: sprintf(root_name,"PVM_L%d_IS%d",L,CONFIGURACAO_I);
-		break;
-       	case 3: sprintf(root_name,"PVM_L%d_IS%d",L,CONFIGURACAO_I);
-		break;
-       }
-
-/* Try to open it for reading (to check if it already exists): */
-ok=0;
-do {
-    sprintf(output_file_1,"%s_%ld_LUCCA.dat",root_name,identifier);
-    fp1 = fopen(output_file_1,"r");
-
-    if (fp1!=NULL) {
-        identifier += 2;
-        fclose(fp1);
+unsigned long generateFile(void){
+  char name[100], context[50];
+  unsigned long seed = (unsigned long) time(NULL);
+  if(seed % 2 == 0) seed += 1;
+  if(FORCE_SEED == 1) seed = (unsigned long) 123456789;
+  switch (CONFIGURACAO_I) {
+    case 1:
+      sprintf(context,"PVM_L%d_IS%d_R%d",L,CONFIGURACAO_I,RAIO);
+  	   break;
+    case 2:
+      sprintf(context,"PVM_L%d_IS%d",L,CONFIGURACAO_I);
+  	  break;
+    case 3:
+      sprintf(context,"PVM_IS%d",CONFIGURACAO_I);
+  		break;
+  }
+  int f = 0;
+  while(f == 0){
+    sprintf(name, "%s_%ld.dat", context, seed);
+    fp1 = fopen(name, "r");
+    if(fp1 != NULL){
+      seed += 2;
     } else {
-        ok = 1;
+      f += 1;
     }
-} while (ok==0);
+    fclose(fp1);
+  }
 
+  sprintf(name, "%s_%ld.dat", context, seed);
+  fp1 = fopen(name, "w");
 
-sprintf(output_file_1,"%s_%ld_LUCCA.dat",root_name,identifier);
-fp1 = fopen(output_file_1,"w");
-fprintf(fp1,"# Persistent Voter Model: pvm.c\n");
-fprintf(fp1,"# Initial state: %d\n",CONFIGURACAO_I);
-fprintf(fp1,"# Linear size: %d",L);
-if (CONFIGURACAO_I==1) fprintf(fp1,"  Radius: %d\n",RAIO);
-                 else fprintf(fp1,"\n");
-fprintf(fp1,"# Maximum simulation time: %d\n",(int)TEMPO_MAX);
-fflush(fp1);
+  fprintf(fp1,"# Persistent Voter Model: pvm.c\n");
+  fprintf(fp1,"# Initial state: %d\n",CONFIGURACAO_I);
+  fprintf(fp1,"# Linear size: %d",L);
+  if (CONFIGURACAO_I==1) fprintf(fp1,"  Radius: %d\n",RAIO);
+                   else fprintf(fp1,"\n");
+  fprintf(fp1,"# Maximum simulation time: %d\n",(int)TEMPO_MAX);
+  fflush(fp1);
 
-return identifier;
+  return seed;
 }
 
-void save_configuration(int qual){
+void save_configuration(int qual, char* name){
 int i;
 char output_file_eps[100];
 
-//sprintf(output_file_eps,"%s_N%.4f_1.eps",root_name,occ);
-//lat2eps_init(LSIZE,LSIZE);
-//lat2eps_set_color(0,0xFFFFFF); // white, empty sites
-//lat2eps_set_color(1,0x9AFF00); // green, S
-//lat2eps_set_color(2,0xFF0000); // red, I
-//lat2eps_set_color(3,0x000000); // black, R
-//for (i=0; i<NSIZE; ++i)
-//    {
-//     if (s[i]==-1) lat2eps_set_site(i%LSIZE,i/LSIZE,3);
-//        else lat2eps_set_site(i%LSIZE,i/LSIZE,s[i]);
-//}
-//lat2eps_gen_eps(output_file_eps,0,0,LSIZE,LSIZE,1,3);
-//lat2eps_release();
-
-sprintf(output_file_eps,"PVM_conf_%06d.eps",qual);
+sprintf(output_file_eps,"PVM_%s_%06d.eps",name, qual);
 lat2eps_init(L, L);
 //lat2eps_set_color(0,0x000000); // black, for s=0, normal sites
 //lat2eps_set_color(1,0x31a2f2); // blue, for s=0, zealot sites
@@ -624,23 +658,145 @@ lat2eps_release();
 return;
 }
 
+void unique_cluster(){
+  int *label, *z0c, *z1c, *arc, *normal_label;
+  int sV1, sV2, zV1, zV2, s0, z0;
+  label = jmalloc(N * sizeof(int));
+  normal_label = jmalloc(N * sizeof(int));
+  z0c = jmalloc(N * sizeof(int));
+  z1c = jmalloc(N * sizeof(int));
+  arc = jmalloc(N * sizeof(int));
+  for (int i = 0; i < N; ++i){
+      label[i] = i;           //label are from cluster with same zealotery and spins
+      normal_label[i] = i;    //normal_label are from clusters with same zealotery
+  }
+
+
+  //diferencia os clusters
+  for (int i = 0; i < N; ++i) {
+    s0 = polaridade0[i]; z0 = zelotes0[i];
+    sV1 = polaridade0[dir[i]]; zV1 = zelotes0[dir[i]];
+    sV2 = polaridade0[baixo[i]]; zV2 = zelotes0[baixo[i]];
+    if(z0 == zV1){
+      unionfind(i, dir[i], normal_label);
+      if(s0 == sV1){
+        unionfind(i, dir[i], label);
+      }
+    }
+    if(z0 == zV2){
+      unionfind(i, baixo[i], normal_label);
+      if(s0 == sV2){
+        unionfind(i, baixo[i], label);
+      }
+    }
+  }
+  if(L == 24){
+    for(int i = 0; i < L; i++){
+      for(int j = 0; j < L; j++){
+        int index = (i * L) + j;
+        printf("%d   ", label[index]);
+      }
+      printf("\n");
+    }
+  }
+
+  int z0_lab = label[0], z1_lab = label[N - 1], s = 0;
+  for(int i = 0; i < N; i++){
+    if(z0_lab == label[i]){
+      z0c[i] = 1;
+    }
+    if(z1_lab == label[i]){
+      z1c[i] = 1;
+      if(L == 16){
+        printf("%d\n", i);
+        s += 1;
+      }
+    }
+  }
+
+  getBiggestCluster(arc, normal_label, 2);
+
+  l0_int = 0;
+  l1_int = 0;
+  l0_int = getInterfacialSizeOfCluster(z0c, arc);
+  l1_int = getInterfacialSizeOfCluster(z1c, arc);
+  free(label);free(z0c); free(z1c);
+}
+
+int getInterfacialSizeOfCluster(int *cluster1, int *cluster2){
+  int l = 0, si, zi, viz, lab;
+  for(int i = 0; i < N; i++){
+    lab = cluster1[i];
+    if(lab != 1) continue;
+    si = polaridade0[i]; zi = zelotes0[i];
+
+    for(int j = 0; j < 4; j++){
+      viz = vizinhos[i][j];
+
+      if(polaridade0[viz] != si || zelotes0[viz] != zi){
+        l += 1;
+      }
+
+
+    }
+  }
+  return l;
+
+}
+
+void getBiggestCluster(int *biggestCluster, int *label, int config){
+  int *clusterSize_arr = jmalloc(N * sizeof(int));
+  int j;
+  for(int i = 0; i < N; i++) clusterSize_arr[i] = 0;
+  for(int i = 0; i < N; i++){
+    j = label[i];
+    if(config > 1){
+      if(zelotes0[i] == 0){
+        clusterSize_arr[j] += 1;
+      }
+    } else {
+      if(polaridade0[i] == config){
+        clusterSize_arr[j] += 1;
+      }
+    }
+  }
+  int clusterSize = 0, cluster_lab = 0;
+  for(int i = 0; i < N; i++){
+    if(clusterSize <= clusterSize_arr[i]){
+      clusterSize = clusterSize_arr[i];
+      cluster_lab = i;
+    }
+  }
+  for(int i = 0; i < N; i++){
+    if(label[i] == cluster_lab){
+      biggestCluster[i] = 1;
+    } else {
+      biggestCluster[i] = 0;
+    }
+  }
+  free(clusterSize_arr);
+
+}
+
 
 void hoshen_kopelman(int *his0, int *numc, int *per){
     int i,j,*label,*siz;
     int temp1,temp2,wtemp1,wtemp2,probpercv0,probpercv1;
 
+    //criar uma lista label de 0 a N identificando clusters
+    //a lista siz guarda o tamanho da cluster i-esima
     label = jmalloc(N * sizeof(int));
     siz = jmalloc(N * sizeof(int));
     for (i = 0; i < N; ++i){
         label[i] = i;
         siz[i] = 0;
-    //his0[i] = 0;
     }
-    //his0[NSIZE] = 0;
+
     probpercv0=0;
     probpercv1=0;
 
-    for (i = 0; i < N; ++i) { /* Build the domains */
+    //diferencia os clusters
+    for (i = 0; i < N; ++i) {
         if ( (2 * polaridade0[i] + zelotes[i]) == (2 * polaridade0[dir[i]] + zelotes0[dir[i]])) unionfind(i, dir[i], label);
         if ( (2 * polaridade0[i] + zelotes[i]) == (2 * polaridade0[baixo[i]] + zelotes0[baixo[i]])) unionfind(i, baixo[i], label);
     }
@@ -665,7 +821,6 @@ void hoshen_kopelman(int *his0, int *numc, int *per){
                 if (polaridade0[i] == 0) probpercv0 += percolates2d(i, label);
                 if (polaridade0[i] == 1) probpercv1 += percolates2d(i, label);
             }
-            //++his0[siz[i]];
             if (siz[i] >= temp1) {
                 temp2 = temp1;
                 temp1 = siz[i];
@@ -693,24 +848,12 @@ void hoshen_kopelman(int *his0, int *numc, int *per){
     free(label);
     return;
 }
+
+
+
 int percolates2d(int qual,int *lab) {
     int ok1=0,ok2=0;
     int i,j;
-
-    //for (i=0; i<LSIZE; ++i) /* check one direction */
-    //    {
-    //     ok1=0;
-    //     for (j=i; j<NSIZE; j+=LSIZE)
-    //         {
-    //	  if (lab[j]==qual) {
-    //	                     ok1=1;
-    //                             break;
-    //	                    }
-    //	 }
-    //     if (ok1==0) break;
-    //    }
-    /* if spans on the previous direction, ok1=1, otherwise ok1=0, but we need
-    to check the perpendicular direction: */
     for (i = 0; i < N; i += L){
         ok2 = 0;
         for (j = i; j < i + L; ++j){
